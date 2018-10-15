d0569e778d28545e09a163887bd4d4f53babe2bd
(s) Eugenio Guarino <eugenio.guarino@students.plymouth.ac.uk>
2018-10-15 10:18:21 +0100

    Generated Override method 'IsInStock' in the 'ServiceStockItem' class

--- Week03/StockTracker/src/stocktracker/StockTracker.java
+++ Week03/StockTracker/src/stocktracker/StockTracker.java
@@ -5,6 +5,9 @@
  */
 package stocktracker;
 
+import stocktracker.stockdatamodel.PhysicalStockItem;
+import stocktracker.stockdatamodel.ServiceStockItem;
+
 /**
  *
  * @author eguarino
@@ -15,7 +18,26 @@ public class StockTracker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a Physical Stock Item object and a Service Stock Item object
+        PhysicalStockItem objPhysicalItem = 
+                new PhysicalStockItem("Snuff: A Diskworld book by Terry Pratchett", 0);
+        ServiceStockItem objVirtualItem =
+                new ServiceStockItem ("Wintersmith: A Diskworld ebook by Terry Pratchett");
+        
+        //Test the behaviour of the Physical Stock Item
+        String strMessage = objPhysicalItem.getName()
+                + ", Is in stock = " + objPhysicalItem.isInStock()
+                + ", Qty in stock: " + objPhysicalItem.getQuantityInStock();
+        System.out.println(strMessage);
+        
+        strMessage = objVirtualItem.getName()
+                + ", Is in stock = " + objVirtualItem.isInStock()
+                + ", Qty in stock: " + objVirtualItem.getQuantityInStock();
+
+        System.out.println(strMessage);
+        
+        
+                
     }
     
 }
